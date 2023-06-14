6debfa85cb131878a6f1a88c1ca36fa7b9071a10
Mark Dowd <10613809@mydbs.ie>
2023-06-14 11:46:26 +0100

    first commit

---
+++ CampingWebsiteAPI/Controllers/AppUserController.cs
@@ -0,0 +1,111 @@
+﻿using CampingWebsiteAPI.Models;
+using CampingWebsiteAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CampingWebsiteAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AppUserController : ControllerBase
+    {
+        private readonly AppUserService _appUserService;
+        private readonly IConfiguration _configuration;
+
+        public AppUserController(AppUserService appUserService, IConfiguration configuration)
+        {
+            _appUserService = appUserService;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                appUser.CreatedAt = DateTime.UtcNow;
+                // Hash the password
+                appUser.Password = BCrypt.Net.BCrypt.HashPassword(appUser.Password);
+                _appUserService.Create(appUser);
+                return Ok();
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequestModel model)
+        {
+            var appUser = _appUserService.FindByEmail(model.Email);
+            if (appUser != null && BCrypt.Net.BCrypt.Verify(model.Password, appUser.Password))
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[] 
+                    {
+                        new Claim(ClaimTypes.Name, appUser.Id.ToString())
+                        // Add other claims as needed
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(7),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var response = new
+                {
+                    UserId = appUser.Id,
+                    UserName = appUser.Name,
+                    CartItemCount = appUser.CartItems.Count, // assuming CartItems is a list or collection
+                    Token = tokenHandler.WriteToken(token)
+                };
+                return Ok(response);
+            }
+
+            return Unauthorized();
+        }
+
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            // Perform any necessary logout actions
+            return Ok();
+        }
+
+        [HttpGet("account/{userId}")]
+        public IActionResult GetAccount(string userId)
+        {
+            var appUser = _appUserService.GetUserById(userId);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(appUser);
+        }
+
+        [HttpPut("account/{userId}")]
+        public IActionResult UpdateAccount(string userId, AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _appUserService.Update(userId, appUser);
+                return Ok();
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+
+    public class LoginRequestModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
---
+++ CampingWebsiteAPI/Program.cs
@@ -0,0 +1,104 @@
+﻿using CampingWebsiteAPI.Models;
+using CampingWebsiteAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CampingWebsiteAPI", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+// Register MongoDBContext in the dependency injection container
+builder.Services.Configure<MongoDBSettings>(
+    builder.Configuration.GetSection(nameof(MongoDBSettings)));
+
+builder.Services.AddSingleton<IMongoDBSettings>(sp =>
+    sp.GetRequiredService<IOptions<MongoDBSettings>>().Value);
+
+builder.Services.AddSingleton<MongoDBContext>();
+
+// Register AppUserService
+builder.Services.AddSingleton<AppUserService>();
+
+// Register CartService
+builder.Services.AddScoped<CartService>();
+
+// Register ProductService
+builder.Services.AddSingleton<ProductService>();
+
+// Configure JWT Authentication
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false; // Set this to true in production
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CampingWebsiteAPI v1");
+        c.RoutePrefix = string.Empty;
+    });
+    app.UseSession();
+}
+
+app.UseAuthentication(); // Add this line
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
